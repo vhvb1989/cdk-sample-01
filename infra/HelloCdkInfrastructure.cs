95729ffa04854d05f5f4769dfa110d2d975f68c3
Victor Vazquez <vhvb1989@gmail.com>
2023-09-12 18:35:53 +0000

    all and lib

---
+++ infra/HelloCdkInfrastructure.cs
@@ -0,0 +1,38 @@
+﻿using Cdk.Core;
+using Cdk.KeyVault;
+using Cdk.ResourceManager;
+using Cdk.Sql;
+using Azure.Core;
+
+namespace Hello.Cdk
+{
+    public class HelloCdkInfrastructure : Infrastructure
+    {
+        public HelloCdkInfrastructure(AzureLocation? location = default)
+        {
+            location ??= AzureLocation.WestUS;
+
+            Parameter sqlAdminPasswordParam = new Parameter("sqlAdminPassword", "SQL Server administrator password", isSecure: true);
+            Parameters.Add(sqlAdminPasswordParam);
+
+            Parameter appUserPasswordParam = new Parameter("appUserPassword", "Application user password", isSecure: true);
+            Parameters.Add(appUserPasswordParam);
+
+            ResourceGroup resourceGroup = new ResourceGroup(Resource.SubscriptionScope, location: location);
+            resourceGroup.Properties.Tags.Add("key", "value");
+            Resources.Add(resourceGroup);
+
+            KeyVault keyVault = new KeyVault(resourceGroup, "hello-adk-kevault", location: location, accessPrincipal: Guid.NewGuid());
+            keyVault.AddOutput("endpoint", nameof(keyVault.Properties.Properties.VaultUri));
+
+            KeyVaultSecret sqlAdminSecret = new KeyVaultSecret(keyVault, "sqlAdminPassword");
+            sqlAdminSecret.AssignParameter(nameof(sqlAdminSecret.Properties.Properties.Value), sqlAdminPasswordParam);
+
+            KeyVaultSecret appUserSecret = new KeyVaultSecret(keyVault, "appUserPassword");
+            appUserSecret.AssignParameter(nameof(appUserSecret.Properties.Properties.Value), appUserPasswordParam);
+
+            SqlServer sqlServer = new SqlServer(resourceGroup, "hello-adk-sqlserver", location: location);
+            sqlServer.AssignParameter(nameof(sqlServer.Properties.AdministratorLoginPassword), sqlAdminPasswordParam);
+        }
+    }
+}
---
+++ src/Cdk.Core/src/keyvault/KeyVault.cs
@@ -0,0 +1,39 @@
+﻿using Cdk.Core;
+using Azure.Core;
+using Azure.ResourceManager.KeyVault;
+using Azure.ResourceManager.KeyVault.Models;
+
+namespace Cdk.KeyVault
+{
+    public class KeyVault : Resource<KeyVaultData>
+    {
+        public override string Name { get; } = $"kv{Infrastructure.Seed}";
+
+        public KeyVault(Resource scope, string name, string version = "2023-02-01", AzureLocation? location = default, Guid? accessPrincipal = default)
+            : base(scope, version, ArmKeyVaultModelFactory.KeyVaultData(
+                name: name is null ? $"kv-{Infrastructure.Seed}" : $"{name}-{Infrastructure.Seed}",
+                resourceType: "Microsoft.KeyVault/vaults",
+                location: location ?? AzureLocation.WestUS,
+                properties: ArmKeyVaultModelFactory.KeyVaultProperties(
+                sku: new KeyVaultSku(KeyVaultSkuFamily.A, KeyVaultSkuName.Standard),
+                accessPolicies: accessPrincipal.HasValue ? new List<KeyVaultAccessPolicy>()
+                {
+                    new KeyVaultAccessPolicy(Guid.Empty, accessPrincipal.Value.ToString(), new IdentityAccessPermissions()
+                    {
+                        Secrets =
+                        {
+                            IdentityAccessSecretPermission.Get,
+                            IdentityAccessSecretPermission.List
+                        }
+                    })
+                } : default)))
+        {
+        }
+
+        public void ParameterizeAccessPolicyObjectId(string instance, Parameter parameter)
+        {
+            ParameterOverrides.Add("objectId", parameter.Name);
+            Parameters.Add(parameter);
+        }
+    }
+}
